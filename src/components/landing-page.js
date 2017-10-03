95b7d6bfe46051e6b750e6d4e3cf5693d6ebbe4a
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-10-03 23:04:27 +0530

    landing-page

---
+++ src/components/landing-page.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import '../index.css';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <header id="landing-page-image">
+
+        </header>
+      </div>
+    )
+  }
+}
+
+export default LandingPage;
